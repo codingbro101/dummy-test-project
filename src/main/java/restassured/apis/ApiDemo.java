09f159430d4b05956b4a53a92e6371a445802c29
pranav <innovativeskull@gmail.com>
2022-03-12 23:53:27 +0530

    ADD: First commit

---
+++ src/main/java/restassured/apis/ApiDemo.java
@@ -0,0 +1,13 @@
+package restassured.apis;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class ApiDemo {
+
+    public void getUser(int userID) {
+        Response response = RestAssured.given().baseUri("https://reqres.in/api/users?page=" + userID).get();
+        System.out.println("API call completed with status code : " + response.getStatusCode());
+        System.out.println("User retrieved : " + response.getBody());
+    }
+}
---
+++ src/test/java/apitesting/ApiTest.java
@@ -0,0 +1,18 @@
+package apitesting;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import restassured.apis.ApiDemo;
+
+public class ApiTest {
+
+    ApiDemo apiDemo = new ApiDemo();
+
+    @Test
+    @Parameters("userID")
+    public void getUser(int userId) {
+        apiDemo.getUser(2);
+    }
+}
